b018ce9e11f78bb86b0b61d5b90d00f92ecee12e
lmoberly1 <lukemoberly@gmail.com>
2022-11-27 22:40:54 -0800

    Add music generation abilities

--- distance_between.py
+++ distance_between.py
@@ -6,97 +6,107 @@ import argparse
 import imutils
 import cv2
 
+
 class DistanceEstimator():
 
-  def __init__(self):
-    
-    self.REF_WIDTH = 3
-  
-  def midpoint(self, ptA, ptB):
-	  return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
+    def __init__(self):
 
-  def get_distance(self, frame):
-    frame_h, frame_w = frame.shape[0:2]
+        self.REF_WIDTH = 3
 
-    # Convert to grayscale and perform Gaussian Blur
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (7, 7), 0)
+    def midpoint(self, ptA, ptB):
+        return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
 
-    # Perform Canny Edge Detection with dilation and erosion
-    edged = cv2.Canny(gray, 50, 100)
-    edged = cv2.dilate(edged, None, iterations=1)
-    edged = cv2.erode(edged, None, iterations=1)
+    def get_distance(self, frame):
+        frame_h, frame_w = frame.shape[0:2]
 
-    # Find contours in the edge map
-    cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
-      cv2.CHAIN_APPROX_SIMPLE)
-    cnts = imutils.grab_contours(cnts)
+        # Convert to grayscale and perform Gaussian Blur
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (7, 7), 0)
 
-    if not cnts:
-      print('no contours')
-      return frame
+        # Perform Canny Edge Detection with dilation and erosion
+        edged = cv2.Canny(gray, 50, 100)
+        edged = cv2.dilate(edged, None, iterations=1)
+        edged = cv2.erode(edged, None, iterations=1)
 
-    # Sort the contours from left-to-right; initialize the distance colors and reference object
-    (cnts, _) = contours.sort_contours(cnts)
-    colors = ((0, 0, 255), (240, 0, 159), (0, 165, 255), (255, 255, 0), (255, 0, 255))
-    refObj = None
+        # Find contours in the edge map
+        cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
+                                cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
 
-    # Loop over the contours individually
-    orig = frame
-    for c in cnts:
-      # Ignore small contours
-      if cv2.contourArea(c) < 100:
-        continue
-      # Compute the rotated bounding box of the contour
-      box = cv2.minAreaRect(c)
-      box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
-      box = np.array(box, dtype="int")
+        if not cnts:
+            print('no contours')
+            return frame
 
-      # Order the points in the contour such that they appear in order:
-      # top-left, top-right, bottom-right, and bottom-left
-      box = perspective.order_points(box)
-      # Compute the center of the bounding box
-      cX = np.average(box[:, 0])
-      cY = np.average(box[:, 1])
+        # Sort the contours from left-to-right; initialize the distance colors and reference object
+        (cnts, _) = contours.sort_contours(cnts)
+        colors = ((0, 0, 255), (240, 0, 159), (0, 165, 255),
+                  (255, 255, 0), (255, 0, 255))
+        refObj = None
 
-      # Set reference object if first contour
-      if refObj is None:
-        # Unpack the ordered bounding box and compute midpoint 
-        # between top-left and top-right points AND between the top-right and bottom-right
-        (tl, tr, br, bl) = box
-        (tlblX, tlblY) = self.midpoint(tl, bl)
-        (trbrX, trbrY) = self.midpoint(tr, br)
-        # Compute the Euclidean distance between the midpoints and construct the reference object
-        D = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
-        refObj = (box, (cX, cY), D / self.REF_WIDTH)
-        continue
-      
-      # Draw the contours on the image
-      orig = frame.copy()
-      cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 2)
-      cv2.drawContours(orig, [refObj[0].astype("int")], -1, (0, 255, 0), 2)
-      # Stack the reference coordinates and the object coordinates to include the object center
-      refCoords = np.vstack([refObj[0], refObj[1]])
-      objCoords = np.vstack([box, (cX, cY)])
+        # Loop over the contours individually
+        orig = frame
+        for c in cnts:
+            # Ignore small contours
+            if cv2.contourArea(c) < 100:
+                continue
+            # Compute the rotated bounding box of the contour
+            box = cv2.minAreaRect(c)
+            box = cv2.cv.BoxPoints(
+                box) if imutils.is_cv2() else cv2.boxPoints(box)
+            box = np.array(box, dtype="int")
 
-      # Loop over the original points
-      for ((xA, yA), (xB, yB), color) in zip(refCoords, objCoords, colors):
-        # Draw circles corresponding to the current points and connect them with a line
-        cv2.circle(orig, (int(xA), int(yA)), 5, color, -1)
-        cv2.circle(orig, (int(xB), int(yB)), 5, color, -1)
-        cv2.line(orig, (int(xA), int(yA)), (int(xB), int(yB)),
-          color, 2)
-        # Compute the Euclidean distance between the coords and 
-        # convert the distance in pixels to distance in units
-        D = dist.euclidean((xA, yA), (xB, yB)) / refObj[2]
-        (mX, mY) = self.midpoint((xA, yA), (xB, yB))
-        cv2.putText(orig, "{:.1f}in".format(D), (int(mX), int(mY - 10)),
-          cv2.FONT_HERSHEY_SIMPLEX, 0.55, color, 2)
-        # show the output image
-        # cv2.imshow("Image", orig)
-        # cv2.waitKey(0)
-        
-    return orig
+            # Order the points in the contour such that they appear in order:
+            # top-left, top-right, bottom-right, and bottom-left
+            box = perspective.order_points(box)
+            # Compute the center of the bounding box
+            cX = np.average(box[:, 0])
+            cY = np.average(box[:, 1])
+
+            # Set reference object if first contour
+            if refObj is None:
+                # Unpack the ordered bounding box and compute midpoint
+                # between top-left and top-right points AND between the top-right and bottom-right
+                (tl, tr, br, bl) = box
+                (tlblX, tlblY) = self.midpoint(tl, bl)
+                (trbrX, trbrY) = self.midpoint(tr, br)
+                # Compute the Euclidean distance between the midpoints and construct the reference object
+                D = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
+                refObj = (box, (cX, cY), D / self.REF_WIDTH)
+                continue
+
+            # Draw the contours on the image
+            orig = frame.copy()
+            cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 2)
+            cv2.drawContours(
+                orig, [refObj[0].astype("int")], -1, (0, 255, 0), 2)
+            # Stack the reference coordinates and the object coordinates to include the object center
+            refCoords = np.vstack([refObj[0], refObj[1]])
+            objCoords = np.vstack([box, (cX, cY)])
+
+            i = 0
+            # Loop over the original points
+            for ((xA, yA), (xB, yB), color) in zip(refCoords, objCoords, colors):
+                if i != 4:  # only get midpoint
+                    i += 1
+                    continue
+                # Draw circles corresponding to the current points and connect them with a line
+                cv2.circle(orig, (int(xA), int(yA)), 5, color, -1)
+                cv2.circle(orig, (int(xB), int(yB)), 5, color, -1)
+                cv2.line(orig, (int(xA), int(yA)), (int(xB), int(yB)),
+                         color, 2)
+                # Compute the Euclidean distance between the coords and
+                # convert the distance in pixels to distance in units
+                D = dist.euclidean((xA, yA), (xB, yB)) / refObj[2]
+                (mX, mY) = self.midpoint((xA, yA), (xB, yB))
+                cv2.putText(orig, "{:.1f}in".format(D), (int(mX), int(mY - 10)),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.55, color, 2)
+                # show the output image
+                # cv2.imshow("Image", orig)
+                # cv2.waitKey(0)
+                i += 1
+
+        return orig
+
 
 """
 # construct the argument parse and parse the arguments
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from vid_manager import VideoManager
+from sound_ops import SoundOperations
 import os
 
 
@@ -6,11 +7,16 @@ class Main():
 
     def __init__(self):
         self.VI_M = VideoManager()
+        self.SOUND = SoundOperations()
 
     def live_estimation(self, webcam_id=0):
         self.VI_M.estimate_vid(webcam_id)
 
+    def music(self):
+        self.SOUND.play_sound()
+
 
 if __name__ == "__main__":
     app = Main()
-    app.live_estimation(0)
+    app.music()
+    # app.live_estimation(0)
